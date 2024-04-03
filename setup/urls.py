ff4c92c41eeffc84dffe81578ecd81665bce9fb9
Luis Felipe Gilisk de Oliveira <felipegilisk@usp.br>
2024-04-03 19:56:06 -0300

    corrigido redirecionamento da página inicial

--- setup/urls.py
+++ setup/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='core/')),
     path('admin/', admin.site.urls),
     path('core/', include('core.urls')),
     path('locacao/', include('locacao.urls')),
